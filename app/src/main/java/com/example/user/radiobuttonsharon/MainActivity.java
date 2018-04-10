823bcdbd3ab038d36603471a8935395e1bcca6d0
SharonDanieli <fam2.danieli@gmail.com>
2018-04-10 15:46:50 +0300

    1st commit

---
+++ app/src/main/java/com/example/user/radiobuttonsharon/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.user.radiobuttonsharon;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+RadioButton red;
+RadioButton green;
+RadioButton blue;
+LinearLayout screen;
+RadioGroup rg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+        red= red.findViewById(R.id.red);
+        green= green.findViewById(R.id.green);
+        blue= blue.findViewById(R.id.blue);
+        screen= screen.findViewById(R.id.screen);
+        rg= rg.findViewById(R.id.rg);
+    }
+    public void Changebackground (View view){
+        if (red.isChecked()) {
+            screen.setBackgroundColor(Color.RED);
+        }
+        if (green.isChecked()) {
+            screen.setBackgroundColor(Color.GREEN);
+        }
+        if (blue.isChecked()) {
+            screen.setBackgroundColor(Color.BLUE);
+        }
+        else{
+            Toast.makeText(this, "Button wasn't pressed", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void setBackgroundColor (View view){
+        rg.clearCheck();
+        screen.setBackgroundColor(Color.WHITE);
+*/
+    }
+
+}
